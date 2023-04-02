89bb1d2c9a73613362f96535836081dd8bd78171
yagox <yago.ramiresx@gmail.com>
2023-04-02 15:26:40 -0300

    feat: formulario de add participante e estilos

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,32 @@
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
 import React from 'react';
 import styles from './styles';
+import { TouchableOpacity } from 'react-native';
 
-const index = () => {
+const Home = () => {
+  const handleParticipantAdd = () => {
+    console.log('Clicou em add');
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento:</Text>
       <Text style={styles.eventDate}>Sexta, 7 de Abril de 2023.</Text>
+      <View style={styles.formContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder='Nome do participante'
+          placeholderTextColor={'#6B6B6B'}
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => handleParticipantAdd()}
+        >
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
-export default index;
+export default Home;
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -18,6 +18,37 @@ const styles = StyleSheet.create({
     color: '#6B6B6B',
     fontSize: 16,
   },
+
+  input: {
+    backgroundColor: '#1F1E25',
+    borderRadius: 5,
+    height: 56,
+    padding: 16,
+    color: '#FFF',
+    flex: 1,
+  },
+
+  formContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    gap: 12,
+    marginTop: 36,
+    marginBottom: 42,
+  },
+
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: '#31CF67',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  buttonText: {
+    color: '#FFF',
+    fontSize: 24,
+  },
 });
 
 export default styles;
